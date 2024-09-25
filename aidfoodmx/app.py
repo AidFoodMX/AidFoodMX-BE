46ac2bb6a11765f7287da8e05804659ce2aa9ca8
Gorchon <a01254831@tec.mx>
2024-09-24 19:17:23 -0600

    DEV: started info

--- aidfoodmx/app.py
+++ aidfoodmx/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify
+from app.routes import register_routes
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to AidfoodMx Backend!"})
+register_routes(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
