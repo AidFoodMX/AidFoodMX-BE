f567c1ae8003df3daf52b06c05401ea42cb19d0c
César Silva <casrr@proton.me>
2024-10-09 16:27:45 -0600

    CORS fix

--- aidfoodmx/app.py
+++ aidfoodmx/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from app.routes import register_routes
 
 app = Flask(__name__)
+
+# Apply CORS to the entire app
+CORS(app)  # This allows CORS for all routes
 
 register_routes(app)
 
